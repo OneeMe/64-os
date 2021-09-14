e23536fb05ddfb467922df2d3d58f9c28708dcd6
onee <onee.me@outlook.com>
2021-09-15 06:28:27 +0800

    feat: use stack

--- asm/boot.asm
+++ asm/boot.asm
@@ -1,22 +1,24 @@
 mov ah, 0x0e
-; first try
-mov al, secreet
+
+mov bp, 0x8000
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+
+pop bx
+mov al, bl
 int 0x10
-; second try
-mov al, [secreet]
+
+pop bx
+mov al, bl
 int 0x10
-; third try
-mov bx, secreet
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
-; fourth try
-mov al, [0x7c1d]
+
+mov al, [0x7ffe]
 int 0x10
 
 jmp $
-secreet:
-    db 'X'
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
