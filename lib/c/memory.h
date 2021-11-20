f29de21484544ef2920eac4d4b3ca0cbb21d0d6b
onee <onee.me@outlook.com>
2021-11-20 16:58:51 +0800

    feat: 实现一个最简单的 malloc 函数

--- lib/c/memory.h
+++ lib/c/memory.h
@@ -7,6 +7,9 @@
 void memory_copy(u8 *source, u8 *dest, int no_bytes);
 void memory_set(u8 *dest, u8 val, u32 len);
 
+/* 实现一个简单的 malloc 函数 */
+u32 kmalloc(u32 size, int align, u32 *phys_addr);
+
 // 获取一个地址中的低、高16位地址
 #define low_16(address) (u16)((address)&0xFFFF)
 #define high_16(address) (u16)(((address) >> 16) & 0xFFFF)
