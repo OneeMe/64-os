029e8463843af33c4e3db66ea0675dbe4e277f64
onee <onee.me@outlook.com>
2021-11-20 17:09:43 +0800

    feat: 扩充内存管理的方式

--- lib/c/memory.c
+++ lib/c/memory.c
@@ -17,7 +17,8 @@ void memory_set(u8 *dest, u8 val, u32 len)
 }
 // 64KB，应该是因为目前 kernel 占用的大小还没有那么大，所以就把 64KB 这里当做一个 heap 来用了
 u32 base_address = 0x10000;
-u32 kmalloc(u32 size, int align, u32 *phys_addr)
+
+u32 kmalloc_ap(u32 size, int align, u32 *phys_addr)
 {
     if (align == 1 && base_address & 0xFFFFF000)
     {
@@ -25,6 +26,42 @@ u32 kmalloc(u32 size, int align, u32 *phys_addr)
         base_address &= 0xFFFFF000;
         base_address += 0x1000;
     }
+    if (phys_addr)
+    {
+        *phys_addr = base_address;
+    }
+    // 保存一下当前的地址
+    u32 return_address = base_address;
+    // 把传入的 size 分配到传入的地址中
+    base_address += size;
+    return return_address;
+}
+
+u32 kmalloc(u32 size)
+{
+    // 保存一下当前的地址
+    u32 return_address = base_address;
+    // 把传入的 size 分配到传入的地址中
+    base_address += size;
+    return return_address;
+}
+u32 kmalloc_a(u32 size, int align)
+{
+    if (align == 1 && base_address & 0xFFFFF000)
+    {
+        // 4KB 对齐一下
+        base_address &= 0xFFFFF000;
+        base_address += 0x1000;
+    }
+    // 保存一下当前的地址
+    u32 return_address = base_address;
+    // 把传入的 size 分配到传入的地址中
+    base_address += size;
+    return return_address;
+}
+
+u32 kmalloc_p(u32 size, u32 *phys_addr)
+{
     if (phys_addr)
     {
         *phys_addr = base_address;
--- lib/c/memory.h
+++ lib/c/memory.h
@@ -8,7 +8,10 @@ void memory_copy(u8 *source, u8 *dest, int no_bytes);
 void memory_set(u8 *dest, u8 val, u32 len);
 
 /* 实现一个简单的 malloc 函数 */
-u32 kmalloc(u32 size, int align, u32 *phys_addr);
+u32 kmalloc(u32 size);
+u32 kmalloc_ap(u32 size, int align, u32 *phys_addr);
+u32 kmalloc_a(u32 size, int align);
+u32 kmalloc_p(u32 size, u32 *phys_addr);
 
 // 获取一个地址中的低、高16位地址
 #define low_16(address) (u16)((address)&0xFFFF)
