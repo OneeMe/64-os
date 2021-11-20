0e07a3dd83b969242ed9ecbb9f0e04957c8a93ad
onee <onee.me@outlook.com>
2021-11-20 15:07:36 +0800

    feat: 额外实现一些 c 标准库的能力

--- lib/c/memory.h
+++ lib/c/memory.h
@@ -1,8 +1,11 @@
 #ifndef MEM_H
 #define MEM_H
 
+#include "../../cpu/types.h"
+
 // 将 source 中的 bytes 复制到 dest 中
-void memory_copy(char *source, char *dest, int no_bytes);
+void memory_copy(u8 *source, u8 *dest, int no_bytes);
+void memory_set(u8 *dest, u8 val, u32 len);
 
 // 获取一个地址中的低、高16位地址
 #define low_16(address) (u16)((address)&0xFFFF)
